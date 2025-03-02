6bbf543856641b4523a44f40dcde70b680a10b69
mrhugo76 <95393872+mrhugo76@users.noreply.github.com>
2025-03-03 00:53:06 +0800

    Update 101firebase.js

--- 101firebase.js
+++ 101firebase.js
@@ -4,13 +4,6 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import { getFirestore, doc, setDoc, getDoc, updateDoc, increment, serverTimestamp, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
         const firebaseConfig = {
-            apiKey: "AIzaSyDeVfwTVQJAmJirX4J1Rmw4yyUOvx1rcCY",
-            authDomain: "petro-7417e.firebaseapp.com",
-            projectId: "petro-7417e",
-            storageBucket: "petro-7417e.appspot.com",
-            messagingSenderId: "710734339424",
-            appId: "1:710734339424:web:9149ad35d8a7a957e1d205",
-            measurementId: "G-29P6068RQ3"
         };
 // 初始化 Firebase
 const app = initializeApp(firebaseConfig);
